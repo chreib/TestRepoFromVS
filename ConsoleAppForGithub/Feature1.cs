e508ca9200f2e41541ddbe9111ebdebf1d55782e
Christoph Eibel <christoph.eibel@kwb.at>
2019-02-18 09:56:44 +0100

    added add

--- ConsoleAppForGithub/Feature1.cs
+++ ConsoleAppForGithub/Feature1.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ConsoleAppForGithub
+﻿namespace ConsoleAppForGithub
 {
     class Feature1
     {
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
