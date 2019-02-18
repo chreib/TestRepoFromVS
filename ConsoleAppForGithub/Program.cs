d13bfa2d26fc7b6dcd7f4861edfc3989dcabe690
chreib <chreib@users.noreply.github.com>
2019-02-18 09:44:17 +0100

    Update Program.cs

--- ConsoleAppForGithub/Program.cs
+++ ConsoleAppForGithub/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppForGithub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Github");
+            Console.WriteLine("");
         }
     }
 }
